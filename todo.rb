6d1cffcfb0cedc2999ea9e866d7cccaa96bb0ddf
Jay C <imjching@hotmail.com>
2015-08-21 12:29:37 +0800

    This is where I'm done for the assessment. :)

--- todo.rb
+++ todo.rb
@@ -6,8 +6,9 @@ require "byebug"
 # puts "Put your application code in #{File.expand_path(__FILE__)}"
 
 def list
-  tasks = TaskController.list
-  tasks.each do |task|
+  list = TaskController.list
+  puts "TODO list:"
+  list.each do |task|
     if task[:completed] == 1
       puts "#{task[:id]}. #{task[:task_name]} --- DONE"
     elsif task[:completed] == 0
@@ -16,37 +17,26 @@ def list
   end
 end
 
-def task(task_id)
-  TaskController.task_name(task_id)
-end
-
-def add(task_name)
-  TaskController.add(task_name)
-end
-
-def delete(task_id)
-  TaskController.delete(task_id)
-end
-
-def complete(task_id)
-  TaskController.update(task_id)
-end
-
-
 input = ARGV
+
 
   if input[0] == "list"
     list
+
   elsif input[0] == "add"
-    add(input[1..-1].join(" "))
+    TaskController.add(input[1..-1].join(" "))
     puts "Appended #{input[1..-1].join(" ")} to your TODO list..."
+
   elsif input[0] == "delete"
-    puts "Deleted #{task(input[1].to_i)} from your TODO list..."
-    delete(input[1].to_i)
+    puts "Deleted #{TaskController.task_name(input[1].to_i)} from your TODO list..."
+    TaskController.delete(input[1].to_i)
+
   elsif input[0] == "complete"
-    complete(input[1].to_i)
+    TaskController.update(input[1].to_i)
     list
+
   else
     "Command unidentified. Please input list, add, delete or complete."
+
   end
 
